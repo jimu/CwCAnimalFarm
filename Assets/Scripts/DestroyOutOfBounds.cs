150b1699c0c201c510876910244a8b26de1d344c
Jim Urbas <git@jimu.net>
2020-07-21 01:06:31 -0500

    [Lesson 2.2] Projectials & prefabs

---
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    float minX = -30f, maxX = 30f, minZ = -30f, maxZ = 30f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < minX || transform.position.x > maxX ||
+            transform.position.z < minZ || transform.position.z > maxZ)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,7 +9,11 @@ public class PlayerController : MonoBehaviour
 
     int minX = -10, maxX = 10, minZ = -10, maxZ = 10;
 
-    [SerializeField] float speed;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float rof = 0.5f;
+    [SerializeField] Transform bulletPrefab;
+    float cooldown = 0f;
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,9 +27,27 @@ public class PlayerController : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        float x = Mathf.Clamp(transform.position.x * h * Time.deltaTime * speed, minX, maxX);
-        float z = Mathf.Clamp(transform.position.z * v * Time.deltaTime * speed, minZ, maxZ);
-        transform.Translate(new Vector3(x, 0f, z));
+        float x = Mathf.Clamp(transform.position.x + h * Time.deltaTime * speed, minX, maxX);
+        float z = Mathf.Clamp(transform.position.z + v * Time.deltaTime * speed, minZ, maxZ);
+        transform.position = new Vector3(x, 0f, z);
 
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log("Hit at " + hit.point);
+            transform.LookAt(hit.point);
+        }
+
+        if (Time.time > cooldown && Input.GetMouseButton(0))
+        {
+            cooldown = Time.time + rof;
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.LookRotation(transform.forward));
     }
 }
