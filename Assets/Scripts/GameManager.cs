bd38f87666573395ecb46034f006286e0a040ca9
Jim Urbas <git@jimu.net>
2020-07-21 02:05:05 -0500

    [Lesson 2.3] Random spawning & camera angle

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    public int animalIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-20f, 20f), 0, 20f),
+              animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
